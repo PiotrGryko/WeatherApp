1f07efd0ffac8e2e2fdd0b76967d4e6e15b097d1
piotr <piotr@localhost.localdomain>
2017-05-20 19:58:16 +0200

    colours

--- app/src/main/java/bepoland/piotr/com/bepolandtest/MainActivity.java
+++ app/src/main/java/bepoland/piotr/com/bepolandtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import bepoland.piotr.com.bepolandtest.app.list.CityListFragment;
 import bepoland.piotr.com.bepolandtest.app.map.MapFragment;
@@ -12,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!isTablet()) {
+
             tabLayout = (TabLayout) findViewById(R.id.tab_layout);
             tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
